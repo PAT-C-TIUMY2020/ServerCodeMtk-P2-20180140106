cf5016110b06bb9c157adbcce27df863538be4e6
dedechndra <73011460+dedechndra@users.noreply.github.com>
2020-10-24 21:38:03 +0800

    Add project files.

---
+++ ServerCodeMtk-P2-20180140106/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceMtk_P1_20180140106;
+using System.ServiceModel.Description;
+using System.ServiceModel.Channels; //mex
+
+namespace ServerCodeMtk_P2_20180140106
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            Uri address = new Uri("http://localhost:8888/Matematika");
+            BasicHttpBinding bind = new BasicHttpBinding();
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika), address);
+                //ALAMAT BASE ADDRESS
+                hostObj.AddServiceEndpoint(typeof(IMatematika), bind, "");
+                //ALAMAT ENDPOINT
+                //wsdl
+                ServiceMetadataBehavior smb = new
+                ServiceMetadataBehavior(); //Service Runtime Player
+                smb.HttpGetEnabled = true; //untuk mengaktifkan wsdl (dibuka saat development, tidak untuk dibuka)
+                hostObj.Description.Behaviors.Add(smb);
+                //mex
+                Binding mexbind =
+                MetadataExchangeBindings.CreateMexHttpBinding();
+                hostObj.AddServiceEndpoint(typeof(IMetadataExchange),
+                mexbind, "mex");
+                hostObj.Open();
+                Console.WriteLine("Server is ready!!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+                 catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
